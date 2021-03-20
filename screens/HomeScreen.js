7467fdc585ded8af4d3f0691c4a4431dcc320a27
Fah Jin <yongfahjin@hotmail.com>
2021-03-20 16:57:34 +0800

    second update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,68 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import { List, Card, Paragraph, Title } from 'react-native-paper';
+import {createStackNavigator} from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30, 
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20
+  },
+  card: {
+    marginTop: 20, 
+    marginLeft: 15, 
+    marginRight: 15,
+  }
+})
+
 
 
 export default function HomeStack() {
     return (
-        <View style ={{ flex: 1, justifyContent:"center", alignItems: "center"}}>
-            <Text>Home!</Text>
-        </View>
+        <Stack.Navigator>
+            <Stack.Screen name = "Welcome Home" component ={HomeScreen}/>
+        </Stack.Navigator>
       
     );
   }
+  function HomeScreen(){
+    return(
+      <SafeAreaView>
+      <ScrollView>
+      <Text style={styles.title}>
+          International π Day
+        </Text>
+
+
+        <Card style={styles.card}>
+          <Card.Content>
+            <Title>Welcome</Title>
+            <Paragraph>Thank you for downloading this app.</Paragraph>
+          </Card.Content>
+          <Card.Cover style={{marginTop: 10, height: 200}} source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Pi_pie2.jpg/240px-Pi_pie2.jpg' }} />
+        </Card>
+        </ScrollView>
+    </SafeAreaView>
+    );
+    }
+    const Homelist = () => {
+      const [expanded, setExpanded] = React.useState(true);
+    
+      const handlePress = () => setExpanded(!expanded);
+
+      return (
+        <List.Section title="Frequently Asked Questions">
+          <List.Accordion
+            title="What is this for?">
+            <List.Item title="We celebrate internation pie day." />
+          </List.Accordion>
+          <List.Accordion
+            title="When will this be?">
+            <List.Item title="Whenever this pandemic is over." />
+          </List.Accordion>
+        </List.Section>
+      );
+    };
